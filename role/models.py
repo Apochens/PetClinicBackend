01bbb63d4d2b5f3f76658b39846e056707ad25a2
Apochens <15309036477@qq.com>
2023-03-26 19:42:27 +0800

    Update authentication and role

---
+++ role/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=10)
+    description = models.CharField(max_length=300)
+    jobs = models.CharField(max_length=100)
+
+
+class Workflow(models.Model):
+    name = models.CharField(max_length=20)
+    process = models.JSONField()  # An array of steps
+
